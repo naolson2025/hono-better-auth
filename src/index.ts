09aa5a1d2c134d5533cd8036f18980cbdde4db4d
Nick Olson <naolson.1819@gmail.com>
2025-07-07 14:24:20 -0500

    added cors & options that better auth recommends

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,23 @@ import { auth } from './lib/auth';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 import { todos } from './routes/todos.routes';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
 app.use('*', logger());
 app.use('*', prettyJSON());
+app.use(
+  '*',
+  cors({
+    origin: 'http://localhost:3000', // replace with your origin
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 app
   .on(['POST', 'GET'], '/api/auth/**', (c) => auth.handler(c.req.raw))
