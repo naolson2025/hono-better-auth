eadac8be12ee67cdfc528f225e2511db508ffe5f
Nick Olson <45021396+naolson2025@users.noreply.github.com>
2025-07-12 06:51:32 -0500

    added createTodoValidator

--- src/validators/create-todo.validator.ts
+++ src/validators/create-todo.validator.ts
@@ -8,3 +8,18 @@ export const createTodoSchema = z
     completed: z.boolean().default(false),
   })
   .strict();
+
+export const createTodoValidator = zValidator(
+  'json',
+  createTodoSchema,
+  (result, c) => {
+    if (!result.success) {
+      return c.json(
+        {
+          errors: result.error.issues.map((issue) => issue.message),
+        },
+        400
+      );
+    }
+  }
+);
